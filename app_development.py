8cd127b4b5cb61fb5d7c5b45ee83b6419c564f53
Duy Nguyen <146409192+dcnguyen060899@users.noreply.github.com>
2024-01-19 14:17:22 -0800

    Update app_development.py

--- app_development.py
+++ app_development.py
@@ -62,7 +62,7 @@ class InformationList(BaseModel):
 # os.environ["ACTIVELOOP_TOKEN"] = ''
 
 # Fetching secrets
-openai_api_key = st.secrets["openai_api_key"]
+openai.api_key = st.secrets["openai_api_key"]
 os.environ['ACTIVELOOP_TOKEN'] = st.secrets["active_loop_token"]
 
 llm = OpenAI(language_model='gpt-4', temperature=.7)
