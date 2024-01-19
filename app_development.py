a411788feea2472ecfe38680291a85a2363ba208
Duy Nguyen <146409192+dcnguyen060899@users.noreply.github.com>
2024-01-19 02:01:52 -0800

    Update app_development.py

--- app_development.py
+++ app_development.py
@@ -32,8 +32,14 @@ from llama_index.vector_stores import DeepLakeVectorStore
 from llama_index.storage.storage_context import StorageContext
 import deeplake
 from llama_index.llms import OpenAI
-from llama_index.tools import QueryEngineTool, ToolMetadata
+from llama_index.tools import FunctionTool, QueryEngineTool, ToolMetadata
 from llama_index.agent import OpenAIAgent
+from llama_index.embeddings import OpenAIEmbedding
+from pydantic import BaseModel
+from llama_index.output_parsers import PydanticOutputParser
+from llama_index.program import MultiModalLLMCompletionProgram
+from llama_index.llms import OpenAI
+from typing import List
 
 def detect_language(text):
   try:
@@ -41,99 +47,21 @@ def detect_language(text):
   except:
     return "unknown"
 
-# Define variable to hold llama2 weights namingfiner
-name = "meta-llama/Llama-2-7b-chat-hf"
-# Set auth token variable from hugging face
-auth_token = ""
+class Information(BaseModel):
+    """Data model for immigration related information"""
+    url: str
+    header: str
+    content: str
 
-@st.cache_resource(hash_funcs={AutoModelForCausalLM: id, AutoTokenizer: id})
-def get_tokenizer_model():
-    global model, tokenizer
+class InformationList(BaseModel):
+    """A list of immigration information for the model to use"""
+    immigration_details: List[Information]
 
-    # Check if the model and tokenizer have already been loaded
-    if 'model' not in globals() or 'tokenizer' not in globals():
-        print("Loading model and tokenizer...")
+# initialize open ai agent model
+openai.api_key = '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'
+os.environ["ACTIVELOOP_TOKEN"] = 'eyJhbGciOiJIUzUxMiIsImlhdCI6MTcwNTAyMTQ0MywiZXhwIjoxNzM2NjQzODI4fQ.eyJpZCI6ImRjbmd1eWVuMDYwODk5In0.jUIzxdEZQhsCffeVslM0o84NcVXUI_fzaZkQuYtH3sRDKqKuuNbCDSq_iBwNdR75Am8zfuYzjEM_eC5B-0DVgw'
 
-        # Define the model name and authentication token
-        name = "meta-llama/Llama-2-7b-chat-hf"
-        auth_token = "your_auth_token_here"
-
-        # Create tokenizer
-        tokenizer = AutoTokenizer.from_pretrained(name, 
-                                                  cache_dir='/content/drive/My Drive/LLM Deployment/LLM Deployment/', 
-                                                  use_auth_token=auth_token)
-
-        # Create model
-        model = AutoModelForCausalLM.from_pretrained(name, 
-                                                     cache_dir='/content/drive/My Drive/LLM Deployment/LLM Deployment/', 
-                                                     use_auth_token=auth_token, 
-                                                     torch_dtype=torch.float16,
-                                                     rope_scaling={"type": "dynamic", "factor": 2}, 
-                                                     load_in_8bit=True)
-    else:
-        print("Model and tokenizer are already loaded.")
-
-    return model, tokenizer
-
-# Get the model and tokenizer
-model, tokenizer = get_tokenizer_model()
-
-
-# Initialize the SimpleInputPrompt with an empty template
-query_wrapper_prompt = SimpleInputPrompt("{query_str} [/INST]")
-
-# Streamlit UI to let the user update the system prompt
-# Start with an empty string or a default prompt
-default_prompt = ""
-user_system_prompt = st.text_area("How can I best assist you?", value="", height=100)
-update_button = st.button('Request')
-
-# Initialize the llm object with a placeholder or default system prompt
-llm = HuggingFaceLLM(
-    context_window=4096,
-    max_new_tokens=1024,
-    system_prompt="",  # Placeholder if your initial prompt is empty
-    query_wrapper_prompt=query_wrapper_prompt,  # Placeholder string
-    model=model,
-    tokenizer=tokenizer
-)
-
-# Function to update the system prompt and reinitialize the LLM with the new prompt
-def update_system_prompt(new_prompt):
-    global llm
-    llm.system_prompt = new_prompt
-
-
-if update_button:
-    # Update the system prompt and reinitialize the LLM
-    update_system_prompt(user_system_prompt)
-    st.success('Requested')
-
-# Create and dl embeddings instance
-embeddings=LangchainEmbedding(
-    HuggingFaceEmbeddings(model_name="all-MiniLM-L6-v2")
-)
-
-# Create new service context instance where llm object is communicate with retrieval augmented system (VectorStoreIndex)
-service_context = ServiceContext.from_defaults(
-    chunk_size=1024,
-    llm=llm,
-    embed_model=embeddings
-)
-
-# And set the service context
-set_global_service_context(service_context)
-
-# Define a directory for storing uploaded files and initialize other components
-UPLOAD_DIRECTORY = "/content/ai-chatbot-immigrant-assistant"
-if not os.path.exists(UPLOAD_DIRECTORY):
-    os.makedirs(UPLOAD_DIRECTORY)
-
-# Create the Streamlit UI components
-st.title('👔 SettleSmart 🧩')
-
-openai.api_key = ''
-os.environ["ACTIVELOOP_TOKEN"] = ''
+llm = OpenAI(language_model='gpt-4', temperature=.7)
 
 reader = DeepLakeReader()
 query_vector = [random.random() for _ in range(1536)]
@@ -143,44 +71,105 @@ documents = reader.load_data(
     limit=5,
 )
 
-llm = OpenAI(temperature=0.8, model="gpt-4")
-service_context = ServiceContext.from_defaults(llm=llm)
-
 dataset_path = 'SettleMind_AIChatbotImmigrantAssistant_Dataset'
 vector_store = DeepLakeVectorStore(dataset_path=dataset_path, overwrite=True)
 storage_context = StorageContext.from_defaults(vector_store=vector_store)
+
 
 index_vector_store = VectorStoreIndex.from_documents(
     documents, 
     storage_context=storage_context)
 
-query_engine = index_vector_store.as_query_engine()
+immigration_query_engine = index_vector_store.as_query_engine(output_cls=InformationList)
 
-query_engine_tools = [
-    QueryEngineTool(
-        query_engine=query_engine,
-        metadata=ToolMetadata(
-            name="consult",
-            description=(
-                "Provides information about Study Permit for international students.\
-                You are a professional international immigration consultant"
-                "Use a detailed plain text question as input to the tool."
-            ),
+immigration_query_engine_tool =QueryEngineTool(
+    query_engine=immigration_query_engine,
+    metadata=ToolMetadata(
+        name="immigration_query_engine_tool",
+        description=(
+            "This tool is designed to assist with immigration-related queries, "
+            "specifically focusing on study permits, visa applications, and university enrollment procedures for Canada. "
+            "It can provide detailed steps, required documents, application deadlines, and eligibility criteria. "
+            "Usage: input a query like 'How do I apply for a study permit in Canada?' or 'What are the requirements for a university application in Canada?'. "
+            "Use this tool to retrieve specific policy information or procedural steps related to immigration and education in Canada."
         ),
     ),
-]
+)
 
-agent = OpenAIAgent.from_tools(tools=query_engine_tools, verbose=True)
+# Define a Pydantic model for the immigration response
+class ImmigrationResponse(BaseModel):
+    document_requirements: str = ""
+    application_steps: str = ""
+    cultural_tips: str = ""
+    language_support: str = ""
+    additional_info: str = ""
 
-uploaded_file = st.file_uploader("Upload PDF", type="pdf", accept_multiple_files=True)
-upload_button = st.button('Upload')
+def immigration_assistance(nationality: str, user_input: str):
+    """
+    Given the nationality of a user and their specific query, this function returns relevant immigration assistance information.
+    This tool is designed to provide multilingual support for immigration-related queries, 
+    focusing on study permits, visa applications, and general immigration advice for Canada.
 
-if uploaded_file and upload_button:
-  for file in uploaded_file:
-  # Save the uploaded PDF to the directory
-    with open(os.path.join(UPLOAD_DIRECTORY, file.name), "wb") as f:
-      f.write(file.getbuffer())
-    st.success("File uploaded successfully.")
+    Parameters:
+    nationality (str): The nationality of the user or their preferred language for communication.
+    user_input (str): The specific query or request from the user.
+
+    Returns:
+    response: The assistance information related to the user's query.
+
+    Example:
+    >>> immigration_assistance("Brazilian", "How do I apply for a study permit in Canada?")
+    """
+
+    # Define the GPT-4 model
+    gpt4_language_model = OpenAI(language_model="gpt-4")
+
+    # Define prompt template based on nationality and user query
+    prompt_template_str = f"""
+    You are an expert in Canadian immigration policies and fluent in multiple languages, including the language preferred by someone from {nationality}.
+    Provide detailed information in a clear and user-friendly manner about the following query:
+
+    "{user_input}"
+
+    The response should be tailored to the nationality and language preferences of the user. 
+    """
+
+    # Create a function to generate response based on the user's query and nationality
+    translation_completion_program = MultiModalLLMCompletionProgram.from_defaults(
+        output_parser=PydanticOutputParser(ImmigrationResponse),
+        prompt_template_str=prompt_template_str,
+        llm=gpt4_language_model,
+        verbose=True,
+    )
+    response = translation_completion_program()
+    
+    return response
+
+# Tool for immigration assistance based on nationality and user query
+immigration_assistance_tool = FunctionTool.from_defaults(fn=immigration_assistance)
+
+service_context = ServiceContext.from_defaults(
+    chunk_size=1024,
+    llm=llm
+    )
+
+# And set the service context
+set_global_service_context(service_context)
+
+agent = OpenAIAgent.from_tools(
+  system_prompt="""You are an advanced AI trained to assist with immigration-related queries by accessing a specialized vector database. 
+  Your role is to provide accurate and updated information from this database regarding study permits, visa applications, and university enrollment procedures for Canada.
+   When users ask questions, you will directly consult the vector database to find the most relevant and current information available.
+    Your responses should be based on the data stored in this database, ensuring they are precise and tailored to the users' needs.""",
+  tools=[
+        immigration_query_engine_tool,
+        immigration_assistance_tool,
+    ],
+  llm=llm, 
+  verbose=True)
+
+# Create the Streamlit UI components
+st.title('👔 SettleSmart 🧩')
 
 # Session state for holding messages
 if 'messages' not in st.session_state:
@@ -192,52 +181,13 @@ for message in st.session_state.messages:
 
 prompt = st.chat_input('Input your prompt here')
 
-# Initialize a flag in Streamlit's session state to track if files are processed
-if 'uploaded_files_processed' not in st.session_state:
-    st.session_state.uploaded_files_processed = False
-
-# Handling user input and file upload
 if prompt:
-    if uploaded_file and not st.session_state.uploaded_files_processed:
-        # # Process the uploaded file(s)
-        # for file in uploaded_file:
-        #     with open(os.path.join(UPLOAD_DIRECTORY, file.name), "wb") as f:
-        #         f.write(file.getbuffer())
+   # Directly query the OpenAI Agent
+   st.chat_message('user').markdown(prompt)
+   st.session_state.messages.append({'role': 'user', 'content': prompt})
 
-        # Process documents and set up the query engine
-        documents = SimpleDirectoryReader(UPLOAD_DIRECTORY).load_data()
-        index = VectorStoreIndex.from_documents(documents)
-        query_engine = index.as_query_engine(streaming=True, similarity_top_k=1)
+   response = agent.chat(prompt)
+   final_response = response.response
 
-        st.chat_message('user').markdown(prompt)
-        st.session_state.messages.append({'role': 'user', 'content': prompt})
-
-        # Query using Llama 7b indexed documents
-        response = query_engine.query(prompt)
-
-        st.chat_message('assistant').markdown(response)
-        st.session_state.messages.append({'role': 'assistant', 'content': response})
-
-        # Iterate over the response generator and concatenate the data
-        response_content = ""
-        for text in response.response_gen:
-          response_content += text
-
-        response = agent.chat(response_content)
-        
-        # Mark the uploaded files as processed
-        st.session_state.uploaded_files_processed = True
-
-    else:
-        # Directly query the OpenAI Agent
-        st.chat_message('user').markdown(prompt)
-        st.session_state.messages.append({'role': 'user', 'content': prompt})
-
-        response = agent.query(prompt)
-
-        st.chat_message('assistant').markdown(response)
-        st.session_state.messages.append({'role': 'assistant', 'content': response})
-
-    # Reset the upload button state
-    if upload_button:
-        st.session_state.uploaded_files_processed = False
+   st.chat_message('assistant').markdown(final_response)
+   st.session_state.messages.append({'role': 'assistant', 'content': final_response})
