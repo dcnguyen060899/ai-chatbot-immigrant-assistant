3ad7508ca8110cf4c5d45451fde20d1ddb53b929
Duy Nguyen <146409192+dcnguyen060899@users.noreply.github.com>
2024-01-19 22:38:05 -0800

    Update app_development.py

--- app_development.py
+++ app_development.py
@@ -65,7 +65,7 @@ openai.api_key = st.secrets["openai_api_key"]
 os.environ['ACTIVELOOP_TOKEN'] = st.secrets["active_loop_token"]
 # os.environ['OPENAI_API_KEY'] = st.secrets['openai_api_key']
 
-llm = OpenAI(model='gpt-4', temperature=.7)
+llm = OpenAI(model='gpt-3.5-turbo', temperature=.7)
 
 reader = DeepLakeReader()
 query_vector = [random.random() for _ in range(1536)]
@@ -82,7 +82,7 @@ storage_context = StorageContext.from_defaults(vector_store=vector_store)
 
 index_vector_store = VectorStoreIndex.from_documents(
     documents, 
-    storage_context=storage_context,)
+    storage_context=storage_context)
 
 immigration_query_engine = index_vector_store.as_query_engine(output_cls=InformationList)
 
