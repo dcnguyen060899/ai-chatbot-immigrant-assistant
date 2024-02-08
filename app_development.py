ba0070177db912773d7e807ba0848f022a4ec5e6
Duy Nguyen <146409192+dcnguyen060899@users.noreply.github.com>
2024-02-07 16:13:55 -0800

    Update app_development.py

--- app_development.py
+++ app_development.py
@@ -76,7 +76,7 @@ documents = reader.load_data(
 )
 
 dataset_path = 'SettleMind_Test3'
-vector_store = deeplake.dataset(f"hub://dcnguyen060899/{dataset_path}", overwrite=True)
+vector_store = deeplake.load('hub://dcnguyen060899/SettleMind_Test3')
 storage_context = StorageContext.from_defaults(vector_store=vector_store)
 
 
